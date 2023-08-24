2c02b46576227bea1ecc75a183fa4e5696f33557
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-08-24 10:01:56 +0600

    create api and category data page

---
+++ src/components/HomePage/SingleCategory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SingleCategory = ({ category }) => {
+  const { id, features, title, price, categoryId } = category;
+  return <div></div>;
+};
+
+export default SingleCategory;
